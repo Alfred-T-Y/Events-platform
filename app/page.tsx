01b3ee7ca8f10e587bb9d28a056825ee86615237
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2026-06-12 15:38:55 +0100

    light rays added to the background

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Page = () => {
   return (
-    <div>Page</div>
+    <section>
+      <h1 className="text-center">The Hub for every Dev <br/> Event you can't miss</h1>
+      <p className="text-center mt-5">Discover the latest and greatest developer events around the world.</p>
+    </section>
+    
   )
 }
 
